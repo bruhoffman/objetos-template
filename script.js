d694df27f5c446b928e7cd6e2468e6ad0cb4440f
bruhoffman <bruhoffman@gmail.com>
2023-11-21 16:49:28 -0300

    [Objetos] aula-pratica

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-function imprimirObjeto () {
-    //digite a sua solução aqui
+function imprimirObjeto() {
+    //Prática 1
+    const estudante = {
+        nome: "Bruno", // String
+        sobrenome: "Carlos", // String
+        numeroMatricula: 41128741, // Number
+        notasSemestre: [8.5, 9, 7.8, 8] // Array
+    }
+    console.log(estudante);
+
+    //Prática 2
+    estudante.modulo = "Módulo 1 - Fundamentos";
+    console.log(estudante);
+    console.log(estudante.nome);
+    console.log(estudante.notasSemestre[1]);
+    console.log(estudante.modulo);
+
+    //Prática 3
+    const estudanteCopia = {
+        ...estudante
+    }
+    console.log(estudanteCopia);
+    estudanteCopia.nome = "Astrodev";
+    estudanteCopia.notasSemestre.push(9);
+    estudanteCopia.modulo = "Módulo 2 - Frontend";
+    console.log(estudanteCopia);
+
+    const estudanteLabenu = [estudante, estudanteCopia];
+    console.log(estudanteLabenu);
 }
 imprimirObjeto()
